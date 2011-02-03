bc8e02e0acdf20d3aea3ffb4c6d466597d3dd049
Jochen Meyer <derjochenmeyer@gmail.com>
2011-02-03 12:35:57 +0100

    Added a new useless function to somephp.php

--- somephp.php
+++ somephp.php
@@ -6,3 +6,18 @@
  */
 
 print 'Hello World!';
+
+/**
+ * A new function that does something useless.
+ */
+function do_something_useless() {
+  // Set a useless variable.
+  $variable = TRUE;
+  // Waste time with a useless controll statement.
+  if ($variable == TRUE) {
+    // Set useless variable to FALSE.
+    $variable = FALSE;
+  }
+  // Return the useless variable.
+  return $variable;
+}
